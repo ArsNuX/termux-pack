62e71cac058b9908257442e0b27792232407cafb
ArsNuX <79638977+ArsNuX@users.noreply.github.com>
2021-02-25 19:06:35 +0700

    Update install-pkg.sh

--- install-pkg.sh
+++ install-pkg.sh
@@ -23,178 +23,93 @@ echo -e $cyan "Telegram      : t.me/ArsNuX"
 echo
 echo -e $red "-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-"
 echo -e $white
-pkg install python
-y
-pkg install python2
-y
-pkg install git
-y
-pkg install nano
-y
-pkg install figlet
-y
-pkg install wget
-y
-pkg install ruby
-y
-apt update
-y
-apt upgrade
-y
-apt-get update
-y
-apt-get upgrade
-y
-apt install php
-y
-apt install python
-y
-apt install python2
-y
-apt install git
-y
-apt install golang
-y
-apt install host 
-y
-apt install nano
-y
-apt install havij 
-y
-apt install hydra
-y
-apt install wireshark 
-y
-apt install cmatrix
-y
-pkg install figlet
-y
-pkg install wget
-y
-pkg install wget
-y
-pkg install python2
-y
-pkg install python2-dev
-y
-apt install wireshark 
-y
-pkg install cowsay
-y
-pkg install toilet
-y
-pkg install ruby
-y
-pkg install help 
-y
-gem install lolcat
-y
-pkg install curl
-y
-pkg install wgetrc 
-y
-pkg install unzip
-y
-pkg install openssh
-y
-pkg install tor
-y
-pkg install tar
-y
-pkg install zip
-y
-pkg install net-tools
-y
-pkg install unrar
-y
-pkg install clang
-y
-pkg install w3m
-y
-pkg install proot
-y
-pip2 install wget
-y
-pip2 install requests
-y
-apt update
-y
-apt upgrade
-y
-apt install git
-y
-apt install Python
-y
-apt install Python2
-y
-apt installe Python3
-y
-apt install nano
-y
-apt install Python pipeapt install ruby
-y
-pip install bundle
-y
-apt-get install gcc
-y
-apt-get install Build- essential
-y
-apt-get install perl
-y
-apt-get install git
-y
-pkg update
-y
-pkg upgrade
-y
-apt update && apt upgrade
-y
-apt install git
-y
-apt install python3
-y
-apt install python
-y
-apt install ruby
-y
-apt install wget
-y
-apt install unzip
-y
-apt install zip
-y
-apt install nano
-y
-apt install perl
-y
-apt install curl
-y
-apt install proot
-y
-apt update && apt upgrade 
-y
-pkg install git python2 php curl
-y
-pkg install grep 
-y
-apt-get install python3
-y
-pip3 install requests bs4
-y
-apt-get install git
-y
-pkg install git python python2 unzip nano
-y
-pip install requestes
-y
-get update
-y
-apt-get install python 
-y
-apt-get install python
-y
-apt-get install git
-y
+pkg install python -y
+pkg install python2 -y
+pkg install git -y
+pkg install nano -y
+pkg install figlet -y
+pkg install wget -y
+pkg install ruby -y
+apt update -y
+apt upgrade -y
+apt-get update -y
+apt-get upgrade -y
+apt install php -y
+apt install python -y
+apt install python2 -y
+apt install git -y
+apt install golang -y
+apt install host -y
+apt install nano -y
+apt install havij -y
+apt install hydra -y
+apt install wireshark -y
+apt install cmatrix -y
+pkg install figlet -y
+pkg install wget -y
+pkg install wget -y
+pkg install python2 -y
+pkg install python2-dev -y
+apt install wireshark -y
+pkg install cowsay -y
+pkg install toilet -y
+pkg install ruby -y
+pkg install help -y
+gem install lolcat -y
+pkg install curl -y
+pkg install wgetrc -y
+pkg install unzip -y
+pkg install openssh -y
+pkg install tor -y
+pkg install tar -y
+pkg install zip -y
+pkg install net-tools -y
+pkg install unrar -y
+pkg install clang -y
+pkg install w3m -y
+pkg install proot -y
+pip2 install wget -y
+pip2 install requests -y
+apt update -y
+apt upgrade -y
+apt install git -y
+apt install Python -y
+apt install Python2 -y
+apt install Python3 -y
+apt install nano -y
+apt install Python pip -y
+apt install ruby -y
+pip install bundle -y
+apt-get install gcc -y
+apt-get install Build- essential -y
+apt-get install perl -y
+apt-get install git -y
+pkg update -y
+pkg upgrade -y
+apt update && apt upgrade -y
+apt install git -y
+apt install python3 -y
+apt install python -y
+apt install ruby -y
+apt install wget -y
+apt install unzip -y
+apt install zip -y
+apt install nano -y
+apt install perl -y
+apt install curl -y
+apt install proot -y
+apt update && apt upgrade -y
+pkg install git python2 php curl -y
+pkg install grep -y
+apt-get install python3 -y
+pip3 install requests bs4 -y
+apt-get install git -y
+pkg install git python python2 unzip nano -y
+pip install requestes -y
+get update -y
+apt-get install python -y
+apt-get install python -y
+apt-get install git -y
 echo -e $green
 figlet Completed
 echo -e $white
